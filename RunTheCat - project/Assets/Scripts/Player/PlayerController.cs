ccfe8597c4e43fcbb896994f1b6b89ea39664495
Emilia <szymanska.emilka@gmail.com>
2014-01-14 20:23:25 +0100

    Player control finished.

--- RunTheCat - project/Assets/Scripts/Player/PlayerController.cs
+++ RunTheCat - project/Assets/Scripts/Player/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public static PlayerController instance;
     public float moveSpeed = 10f;
     public float moveForce = 200f;
+    public string mapLayerName = "Map";
     public PlayerState playerState { get; set; }
 
     private Transform groundCheck;
@@ -32,8 +33,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerState.isGrounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Default"));
 
+        playerState.isGrounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer(mapLayerName));
+        Debug.Log("Nie spadam");
+        if (!playerState.isGrounded)
+        {
+            Debug.Log("Spadam isgrounded:" + playerState.isGrounded.ToString());
+            var translation = -Time.deltaTime * moveSpeed;
+            transform.Translate(0, translation, 0);
+        }
         if (Input.GetKey("a"))
         {
             Debug.Log("A");
@@ -43,6 +51,8 @@ public class PlayerController : MonoBehaviour
         {
             Move(true);
         }
+        else
+            Move(true);
 
     }
 
@@ -57,11 +67,21 @@ public class PlayerController : MonoBehaviour
 
     public void Move(bool moveRight)
     {
-
         if ((moveRight && !playerState.isFacingRight) || (!moveRight && playerState.isFacingRight))
             Flip();
-        var translation = Time.deltaTime * moveSpeed * (moveRight ? 1 : -1);
-        transform.Translate(translation, 0, 0);
+        //var translation = Time.deltaTime * moveSpeed * (moveRight ? 1 : -1);
+        //transform.Translate(translation, 0, 0);
+        var speed = 10.0;
+		Vector3 dir = Vector3.zero;
+		dir.x = Input.acceleration.x;
+		dir.z = Input.acceleration.z;
+        Debug.Log("x: " + dir.z + " z: " + dir.z);
+        //if (dir.sqrMagnitude > 1)
+        //    dir.Normalize();
+
+        transform.Translate(dir * Time.deltaTime *moveSpeed);
+	
+        //rigidbody2D.AddForce((moveRight ? Vector3.right : Vector3.left) * moveForce);
 
     }
 
