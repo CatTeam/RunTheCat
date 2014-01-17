5f6f586a3d44c3da4647339584e3d3a6a7def472
iroq <michaelszewczak@gmail.com>
2014-01-17 17:31:23 +0100

    Created map collider boundary prefab.

---
+++ RunTheCat - project/Assets/Editor/BoundaryEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(ColliderBoundary))]
+public class BoundaryEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        ColliderBoundary Target = (ColliderBoundary)target;
+
+        Target.size = EditorGUILayout.Vector2Field(
+            "Boundary Size", Target.size);
+
+        Target.thickness = EditorGUILayout.FloatField(
+            "Boundary Thickness", Target.thickness);
+
+        if (UnityEngine.GUI.changed)
+        {
+            EditorUtility.SetDirty(Target);
+            Target.UpdateLines(Target.thickness, Target.size.x, Target.size.y);
+        }
+    }
+}
---
+++ RunTheCat - project/Assets/Scripts/ColliderBoundary.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderBoundary : MonoBehaviour {
+
+    public float thickness;
+    public Vector2 size;
+    public void UpdateLines(float thickness, float width, float height)
+    {
+        #region Getting GOs
+        GameObject temp;
+        Transform tLeft = transform.Find("Left");
+        if(tLeft==null)
+        {
+            temp = new GameObject("Left", typeof(BoxCollider2D));
+            temp.transform.parent = transform;
+            tLeft=temp.transform;
+        }
+
+        Transform tRight = transform.Find("Right");
+        if (tRight == null)
+        {
+            temp = new GameObject("Right", typeof(BoxCollider2D));
+            temp.transform.parent = transform;
+            tRight=temp.transform;
+        }
+
+        Transform tTop = transform.Find("Top");
+        if (tTop == null)
+        {
+            temp = new GameObject("Top", typeof(BoxCollider2D));
+            temp.transform.parent = transform;
+            tTop=temp.transform;
+        }
+
+        Transform tBottom = transform.Find("Bottom");
+        if (tBottom == null)
+        {
+            temp = new GameObject("Bottom", typeof(BoxCollider2D));
+            temp.transform.parent = transform;
+            tBottom=temp.transform;
+        }
+        #endregion
+
+        #region Getting Components
+        BoxCollider2D Left = tLeft.GetComponent<BoxCollider2D>();
+        if (Left == null)
+        {
+            tLeft.gameObject.AddComponent<BoxCollider2D>();
+            Left = tLeft.GetComponent<BoxCollider2D>();
+        }
+
+        BoxCollider2D Right = tRight.GetComponent<BoxCollider2D>();
+        if (Right == null)
+        {
+            tRight.gameObject.AddComponent<BoxCollider2D>();
+            Right = tRight.GetComponent<BoxCollider2D>();
+        }
+
+        BoxCollider2D Top = tTop.GetComponent<BoxCollider2D>();
+        if (Top == null)
+        {
+            tTop.gameObject.AddComponent<BoxCollider2D>();
+            Top = tTop.GetComponent<BoxCollider2D>();
+        }
+
+        BoxCollider2D Bottom = tBottom.GetComponent<BoxCollider2D>();
+        if (Bottom == null)
+        {
+            tBottom.gameObject.AddComponent<BoxCollider2D>();
+            Bottom = tBottom.GetComponent<BoxCollider2D>();
+        }
+        #endregion
+        
+        Vector3 pos=transform.position;
+        
+        Left.size = new Vector2(thickness, height);
+        Left.transform.position = new Vector3(pos.x - thickness / 2, 
+                                                pos.y - height / 2);
+
+        Right.size = new Vector2(thickness, height);
+        Right.transform.position = new Vector3(pos.x + width + thickness / 2,
+                                                pos.y - height / 2);
+
+        Top.size = new Vector2(width, thickness);
+        Top.transform.position = new Vector3(pos.x + width / 2,
+                                            pos.y + thickness / 2);
+
+        Bottom.size = new Vector2(width, thickness);
+        Bottom.transform.position = new Vector3(pos.x + width / 2,
+                                            pos.y - height - thickness / 2);
+    }
+
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Vector3 pos=transform.position;
+        Gizmos.DrawWireCube(new Vector3(pos.x + size.x / 2, pos.y - size.y / 2),
+                            new Vector3(size.x, size.y));
+    }
+
+}
