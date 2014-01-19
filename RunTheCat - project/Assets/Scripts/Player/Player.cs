38ce97be8f75fc6b96e980889e2b86135c643bdb
iroq <michaelszewczak@gmail.com>
2014-01-19 18:04:29 +0100

    Revamped game movement. Terrain scroll added. Player retraction to given Y added.

--- RunTheCat - project/Assets/Scripts/Player/Player.cs
+++ RunTheCat - project/Assets/Scripts/Player/Player.cs
@@ -24,6 +24,7 @@ public class Player : MonoBehaviour
         PlayerController.instance.RestartPosition();
     }
 
+
     public void GameOver()
     {
         //TODO: GameOver action.
---
+++ RunTheCat - project/Assets/Scripts/PlayerVisibility.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerVisibility : MonoBehaviour {
+
+    void OnBecameVisible()
+    {
+        Debug.Log("Appeared!");
+    }
+
+    void OnBecameInvisible()
+    {
+        Debug.Log("Disappeared!");
+        Player.instance.GameOver();
+    }
+
+}
